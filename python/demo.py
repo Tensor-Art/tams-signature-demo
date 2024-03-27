a0b865035413e40c580abb8d6465ec895774e541
lijian <1053294286@qq.com>
2024-03-27 14:24:31 +0800

    mod: add workflow template

--- python/demo.py
+++ python/demo.py
@@ -4,10 +4,16 @@ from generate_signature import generate_signature
 import hashlib
 import time
 
-url_pre = "endpoint from your app"
-url = "/v1/jobs"
+url_pre = "https://" + "endpoint from your app"
 app_id = "your app id"
+
+# u can put ur private key under the root directory
 private_key_path = "../private_key.pem"
+
+url_local = "http://localhost:8080"
+url_workflow = '/v1/workflows'
+url_job = "/v1/jobs"
+url_resource = "/v1/resource"
 
 
 # 文生图
@@ -106,10 +112,10 @@ def img2img(img_path):
 def get_job_result(job_id):
     while True:
         time.sleep(1)
-        response = requests.get(f"{url_pre}{url}/{job_id}", headers={
+        response = requests.get(f"{url_pre}{url_job}/{job_id}", headers={
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': generate_signature("GET", f"{url}/{job_id}", "", app_id, private_key_path)
+            'Authorization': generate_signature("GET", f"{url_job}/{job_id}", "", app_id, private_key_path)
         })
         get_job_response_data = json.loads(response.text)
         if 'job' in get_job_response_data:
@@ -127,8 +133,8 @@ def get_job_result(job_id):
 
 def create_job(data):
     body = json.dumps(data)
-    auth_header = generate_signature("POST", url, body, app_id, private_key_path)
-    response = requests.post(f"{url_pre}{url}", json=data, headers={
+    auth_header = generate_signature("POST", url_job, body, app_id, private_key_path)
+    response = requests.post(f"{url_pre}{url_job}", json=data, headers={
         'Content-Type': 'application/json',
         'Accept': 'application/json',
         'Authorization': auth_header
@@ -143,10 +149,10 @@ def upload_img(img_path):
         "expireSec": 3600,
     }
     body = json.dumps(data)
-    response = requests.post(f"{url_pre}/v1/resource/image", json=data, headers={
+    response = requests.post(f"{url_pre}{url_resource}/image", json=data, headers={
         'Content-Type': 'application/json',
         'Accept': 'application/json',
-        'Authorization': generate_signature("POST", f"/v1/resource/image", body, app_id, private_key_path)
+        'Authorization': generate_signature("POST", f"{url_resource}/image", body, app_id, private_key_path)
     })
     print(response.text)
     response_data = json.loads(response.text)
@@ -154,15 +160,84 @@ def upload_img(img_path):
     put_url = response_data['putUrl']
     headers = response_data['headers']
     with open(img_path, 'rb') as f:
-        files = {'file': f}
         res = f.read()
         response = requests.put(put_url, data=res, headers=headers)
         print(response.text)
     return resource_id
 
 
+def get_workflow_template(template_id):
+    response = requests.get(f"{url_pre}{url_workflow}/{template_id}", headers={
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': generate_signature("GET", f"{url_workflow}/{template_id}", "", app_id, private_key_path)
+    })
+    print(response.text)
+
+
+def workflow_template_check():
+    data = {
+        "templateId": "676018193025756628",
+        "fields": {
+            "fieldAttrs": [
+                {
+                    "nodeId": "25",
+                    "fieldName": "image",
+                    "fieldValue": None
+                },
+                {
+                    "nodeId": "27",
+                    "fieldName": "text",
+                    "fieldValue": "1 girl"
+                }
+            ]
+        }
+    }
+    body = json.dumps(data)
+    response = requests.post(f"{url_pre}{url_workflow}/template/check", json=data, headers={
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': generate_signature("POST", f"{url_workflow}/template/check", body, app_id, private_key_path)
+    })
+    print(response.text)
+
+
+def workflow_template_job():
+    data = {
+        "request_id": "9f2bf085998e76acd5c8bc306a4f034f",
+        "templateId": "676018193025756628",
+        "fields": {
+            "fieldAttrs": [
+                {
+                    "nodeId": "25",
+                    "fieldName": "image",
+                    "fieldValue": "f29036b4-ff7b-4394-8c26-aabc1bdae008"
+                },
+                {
+                    "nodeId": "27",
+                    "fieldName": "text",
+                    "fieldValue": "1 girl, amber_eyes"
+                }
+            ]
+        }
+    }
+
+    body = json.dumps(data)
+    response = requests.post(f"{url_pre}{url_job}/workflow/template", json=data, headers={
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': generate_signature("POST", f"{url_job}/workflow/template", body, app_id, private_key_path)
+    })
+    print(response.text)
+
+
 if __name__ == '__main__':
     # 文生图
     # text2img()
     # 图生图
-    img2img("../test.webp")
+    # img2img("../test.webp")
+
+    # 模版相关
+    # get_workflow_template(676018193025756628)
+    # workflow_template_check()
+    workflow_template_job()
